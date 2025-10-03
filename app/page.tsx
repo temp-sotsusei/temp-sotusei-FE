96057bd40966ba38d214fa1fe9a04e7cc43d8b6a
warabimochi <ecccompsenyou@gmail.com>
2025-10-03 15:42:45 +0900

    fix(auth):画面描画を修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { auth0 } from "@/lib/auth0";
 
 export default async function Home() {
-  const { user } = await auth0.getSession();
+  const session = await auth0.getSession();
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <Link href="/auth/login">ログイン</Link>
-      <p>login flag:{user ? "true" : "false"}</p>
-      <p>tokens:{user && JSON.stringify(user)}</p>
+      <p>login flag:{session?.user ? "true" : "false"}</p>
+      <p>tokens:{session?.user && JSON.stringify(session.user)}</p>
     </div>
   );
 }
