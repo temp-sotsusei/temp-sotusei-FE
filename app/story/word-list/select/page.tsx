266ec5d718d52e0fd75ba789e49fc27cee667a44
warabimochi <ecccompsenyou@gmail.com>
2025-10-10 15:18:04 +0900

    feat(wordList):デザイン修正前まで

--- app/story/word-list/select/page.tsx
+++ app/story/word-list/select/page.tsx
@@ -2,7 +2,14 @@ import WordListSelect from "@/layout/WordListSelect";
 import { FC } from "react";
 
 const Page: FC = () => {
-  return <WordListSelect />;
+  // TODO:ワードリストのAPI設計を考える
+  const wordList: string[][] = [
+    ["どうぶつ", "もり", "かぎ", "たからもの"],
+    ["まほうつかい", "ほし", "てがみ", "ねがい"],
+    ["ふね", "そら", "ともだち", "ぼうけん"],
+  ];
+
+  return <WordListSelect wordList={wordList} />;
 };
 
 export default Page;
--- layout/WordListSelect/index.tsx
+++ layout/WordListSelect/index.tsx
@@ -2,12 +2,14 @@
 
 import { FC } from "react";
 import { APP_TITLE } from "@/constants";
-import Swiper from "swiper";
 
 import "swiper/css";
-import { SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const WordListSelect: FC = () => {
+type Props = {
+  wordList: string[][];
+};
+const WordListSelect: FC<Props> = ({ wordList }) => {
   return (
     <>
       <div className="flex items-center justify-center mt-6">
@@ -16,8 +18,17 @@ const WordListSelect: FC = () => {
       <div className="flex items-center justify-center m-20">
         <p className="font-bold">単語リストを選んでね</p>
       </div>
-      <Swiper>
-        <SwiperSlide></SwiperSlide>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView="auto"
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <div className="w-full flex justify-center items-center">
+            <div className="w-1/2 h-60 border">aaa</div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </>
   );
