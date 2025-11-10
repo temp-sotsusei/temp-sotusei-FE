35b44946c573d8b46058d4eabd71849c1b87be70
warabimochi <ecccompsenyou@gmail.com>
2025-11-10 16:36:11 +0900

    feat(save):物語入力画面エラー描画

---
+++ validation/index.tsx
@@ -0,0 +1,5 @@
+export const titleInputValidation = {
+  required: "タイトルは1文字以上入力してください。",
+  minLength: { value: 1, message: "タイトルは1文字以上入力してください。" },
+  maxLength: { value: 15, message: "タイトルは15文字以下にしてください。" },
+};
