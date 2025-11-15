86ba2d3c22e678d1b826b0dee20ce713516e6432
warabimochi <satomi920003@yahoo.co.jp>
2025-11-15 23:45:07 +0900

    feat(wordList): /api/story/chapter/nextと連携・入力文字列のバグ修正

--- layout/WordListSelect/index.tsx
+++ layout/WordListSelect/index.tsx
@@ -41,9 +41,13 @@ type ChaptersPayload = {
     position: number;
   }[];
 };
-type DroppedStrState = {
+// TODO: 共有する型をlayoutから剥がす
+export type DroppedStrState = {
   id: UniqueIdentifier;
   droppedString: string;
+  // MEMO: ユーザ入力値の何番目かを保持する
+  userInputDroppedIndex: number;
+  // MEMO: Nodeが含まれた入力値の何番目か保持している.editor.getText()の都合上使えなさそうなので消去予定
   stringBuildDroppedIndex: number;
   currentChapterDisplayDroppedIndex: number;
 };
@@ -135,6 +139,7 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
             {
               id: active.id,
               droppedString: active.data.current.draggedText,
+              userInputDroppedIndex: chapterText.length,
               stringBuildDroppedIndex: chapterText.length + totalLength,
               currentChapterDisplayDroppedIndex:
                 editor.state.doc.content.size - 1,
@@ -152,6 +157,14 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
           )
           .run();
       } else if (over) {
+        // MEMO: positionがdropされた時、以下のようにover.data.current.positionの値がズレる
+        // MEMO: 表示時に要素のindex番目で振っているからなのでgetTiptapHTMLで新しくpositionを採番することで解決するが、div側表示がズレる可能性がある
+        // drop文字列 0個目 -> -1
+        // drop文字列 1個目 -> 0
+        // drop文字列 2個目 -> +1
+        // drop文字列 3個目 -> +2
+        // HACK: 対処療法としてズレを吸収しておくが、根本的な解決になっていない
+
         // MEMO:入力文字ドロップ時に発火する
         setDroppedStrState((prev) => {
           const filteredState = prev.filter((item) => item.id !== active.id);
@@ -160,6 +173,8 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
             {
               id: active.id,
               droppedString: active.data.current.draggedText,
+              userInputDroppedIndex:
+                over.data.current.position + 1 - droppedStrState.length,
               stringBuildDroppedIndex: over.data.current.position + 2,
               currentChapterDisplayDroppedIndex: over.data.current.position + 2,
             },
@@ -254,7 +269,7 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
     const chapterKeywords = droppedStrState
       .map((droppedStrState) => ({
         keyword: droppedStrState.droppedString,
-        position: droppedStrState.stringBuildDroppedIndex,
+        position: droppedStrState.userInputDroppedIndex,
       }))
       .sort((current, next) => current.position - next.position);
 
@@ -264,13 +279,12 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
     // 無 -> Node
     let renderChapterItem: RenderChapterItem[] = [];
     let currentNodePostion = 0;
+
     chapterKeywords.forEach(({ keyword, position }, index) => {
-      if (
-        currentNodePostion !== position &&
-        chapterText.slice(currentNodePostion, position)
-      ) {
+      const leftText = chapterText.slice(currentNodePostion, position);
+      if (leftText) {
         renderChapterItem.push({
-          text: chapterText.slice(currentNodePostion, position),
+          text: leftText,
           isInputText: true,
         });
       }
@@ -278,15 +292,15 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
         text: keyword,
         isInputText: false,
       });
-      if (
-        index + 1 === MAX_DROPPABLE_ELEMENTS &&
-        chapterText.slice(currentNodePostion)
-      ) {
+
+      const rightText = chapterText.slice(position);
+      if (index + 1 === MAX_DROPPABLE_ELEMENTS && rightText) {
         renderChapterItem.push({
-          text: chapterText.slice(currentNodePostion),
+          text: rightText,
           isInputText: true,
         });
       }
+
       currentNodePostion = position;
     });
 
@@ -307,15 +321,9 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
     setIsPosting(true);
 
     const chapterText = editor.getText({ blockSeparator: "\n" });
-    console.log("chapterText:", chapterText);
-    // console.log("droppedStrState:", droppedStrState);
-    mergeDroppedText(chapterText, droppedStrState);
-    // TODO droppedStrStateのstringBuildDroppedIndexを降順並び替え
-    // 並び替えた後にchapterTextに挿入する
-    // postChapterRequestの引数に渡す
-
+    const mergedText = mergeDroppedText(chapterText, droppedStrState);
     try {
-      await postChapterRequest("abcdef123");
+      await postChapterRequest(mergedText);
     } catch (error) {
       console.error("章の投稿に失敗しました", error);
     } finally {
@@ -349,11 +357,14 @@ const WordListSelect: FC<Props> = ({ nestedWordList }) => {
                   </div>
                   {/* // TODO:久乗にJSX側に変更があったことを伝える */}
                   <div className="border mx-8 h-64 break-words whitespace-pre-wrap">
-                    {chapter.renderChapterItem.map((chapterItem) =>
+                    {chapter.renderChapterItem.map((chapterItem, index) =>
                       chapterItem.isInputText ? (
-                        <span>{chapterItem.text}</span>
+                        <span key={index}>{chapterItem.text}</span>
                       ) : (
-                        <div className="px-2 py-1 border rounded-2xl inline">
+                        <div
+                          key={index}
+                          className="px-2 py-1 border rounded-2xl inline"
+                        >
                           {chapterItem.text}
                         </div>
                       )
--- uitls/mergeDroppedText.ts
+++ uitls/mergeDroppedText.ts
@@ -1,22 +1,30 @@
-import { UniqueIdentifier } from "@dnd-kit/core";
-
-type DroppedTexts = {
-  id: UniqueIdentifier;
-  droppedString: string;
-  stringBuildDroppedIndex: number;
-  currentChapterDisplayDroppedIndex: number;
-}[];
+import { MAX_DROPPABLE_ELEMENTS } from "@/const";
+import { DroppedStrState } from "@/layout/WordListSelect";
 
 export const mergeDroppedText = (
   inputText: string,
-  droppedTexts: DroppedTexts
+  droppedTexts: DroppedStrState[]
 ) => {
   const sortedDroppedTexts = [...droppedTexts].sort((current, next) => {
-    return next.stringBuildDroppedIndex - current.stringBuildDroppedIndex;
+    return current.userInputDroppedIndex - next.userInputDroppedIndex;
   });
 
   let resultText = "";
-  sortedDroppedTexts.forEach((droppedTexts) => {
-    console.log("droppedTexts:", droppedTexts);
+  let currentPointer = 0;
+  sortedDroppedTexts.forEach((droppedText, index) => {
+    const leftText = inputText.slice(
+      currentPointer,
+      droppedText.userInputDroppedIndex
+    );
+    resultText += leftText + droppedText.droppedString;
+
+    if (index + 1 === MAX_DROPPABLE_ELEMENTS) {
+      const rightText = inputText.slice(currentPointer);
+      resultText += rightText;
+    }
+
+    currentPointer = droppedText.userInputDroppedIndex;
   });
+
+  return resultText;
 };
